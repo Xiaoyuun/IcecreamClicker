ba7650bf7ce3a8ba04fd3c00fde4375163f8a7d3
Julia Ruan <juliaruan2004@gmail.com>
2022-05-30 16:01:19 -0400

    changed to react native firebase

---
+++ IceCLicker/src/screens/Scoreboard/styles.js
@@ -0,0 +1,39 @@
+
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderRadius: 20,
+    marginBottom: 700,
+    textAlign: "center"
+  },
+  column: {
+    flex: 1,
+    flexDirection: "column",
+
+  },
+  title: {
+    flex: 1,
+    fontSize: 50,
+    alignSelf: "center",
+  },
+  row: {
+    flex: 1,
+    flexDirection: "row",
+    borderWidth: 3,
+    borderColor: "black",
+    marginLeft: 5,
+    marginRight: 5,
+  },
+  cell: {
+    display: "flex",
+    flex: 1,
+    fontSize: 30,
+    marginLeft: 20,
+    marginRight: 20,
+    fontFamily: "Gill Sans",
+  },
+});
+
+export default styles;
